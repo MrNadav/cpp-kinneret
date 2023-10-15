a5bcea319abe62443f258ab58a90c02ca7e10c64
nadav <nadav15122003@gmail.com>
2023-10-15 13:24:23 +0300

    finished 2

--- hw3/hw3.cpp
+++ hw3/hw3.cpp
@@ -1,82 +1,9 @@
 #include "Trip.h"
 #include <iostream>
 using namespace std;
-bool IsEmpty(int size) {
-    return size == 0;
-}
-
-void Print(Trip* arr, int size) {
-    if (IsEmpty(size)) {
-        cout << "No trips available." <<   endl;
-        return;
-    }
-
-    for (int i = 0; i < size; i++) {
-        arr[i].printTrip();
-        cout << "--------------------------" <<   endl;
-    }
-}
-
-void EditTrip(Trip* arr, int size) {
-    if (IsEmpty(size)) {
-        std::cout << "No trips available." << std::endl;
-        return;
-    }
-
-    int tripNum;
-    std::cout << "Enter trip number for editing: ";
-    std::cin >> tripNum;
-
-    for (int i = 0; i < size; i++) {
-        if (arr[i].getTripNum() == tripNum) {
-            char dest[100];
-            int day, month, year;
-
-            std::cout << "Enter new destination: ";
-            std::cin >> dest;
-            arr[i].setDest(dest);
-
-            std::cout << "Enter new day: ";
-            std::cin >> day;
-            std::cout << "Enter new month: ";
-            std::cin >> month;
-            std::cout << "Enter new year: ";
-            std::cin >> year;
-            arr[i].setDate(Date(day, month, year));
-
-            return;
-        }
-    }
-    std::cout << "Trip not found!" << std::endl;
-}
 
 
-Trip* AddTrip(Trip* arr, int &size) {
-    char dest[100];
-    int tripNum, day, month, year;
 
-    std::cout << "Enter trip number: ";
-    std::cin >> tripNum;
-
-    std::cout << "Enter destination: ";
-    std::cin >> dest;
-
-    std::cout << "Enter day: ";
-    std::cin >> day;
-    std::cout << "Enter month: ";
-    std::cin >> month;
-    std::cout << "Enter year: ";
-    std::cin >> year;
-
-    Trip* newArr = new Trip[size + 1];
-    for (int i = 0; i < size; i++) {
-        newArr[i] = arr[i];
-    }
-    newArr[size] = Trip(tripNum, dest, Date(day, month, year));
-    delete[] arr;
-    size++;
-    return newArr;
-}
 
 
 int main() {
@@ -108,3 +35,88 @@ int main() {
         }
     } while (option != 4);
 }
+
+
+
+
+
+
+
+
+
+bool IsEmpty(int size) {
+    return size == 0;
+}
+void Print(Trip* arr, int size) {
+    if (IsEmpty(size)) {
+        cout << "No trips available." <<   endl;
+        return;
+    }
+
+    for (int i = 0; i < size; i++) {
+        arr[i].printTrip();
+        cout << "--------------------------" <<   endl;
+    }
+}
+void EditTrip(Trip* arr, int size) {
+    if (IsEmpty(size)) {
+        cout << "No trips available." << endl;
+        return;
+    }
+
+    int tripNum;
+    cout << "Enter trip number for editing: ";
+    cin >> tripNum;
+
+    for (int i = 0; i < size; i++) {
+        if (arr[i].getTripNum() == tripNum) {
+            char dest[100];
+            int day, month, year;
+
+            cout << "Enter new destination: ";
+            cin >> dest;
+            arr[i].setDest(dest);
+
+            cout << "Enter new day: ";
+            cin >> day;
+            cout << "Enter new month: ";
+            cin >> month;
+            cout << "Enter new year: ";
+            cin >> year;
+            arr[i].setDate(Date(day, month, year));
+
+            return;
+        }
+    }
+    cout << "Trip not found!" << endl;
+}
+
+
+Trip* AddTrip(Trip* arr, int &size) {
+    char dest[100];
+    int tripNum, day, month, year;
+
+    cout << "Enter trip number: ";
+    cin >> tripNum;
+
+    cout << "Enter destination: ";
+    cin >> dest;
+
+    cout << "Enter day: ";
+    cin >> day;
+    cout << "Enter month: ";
+    cin >> month;
+    cout << "Enter year: ";
+    cin >> year;
+
+    Trip* newArr = new Trip[size + 1];
+    for (int i = 0; i < size; i++) {
+        newArr[i] = arr[i];
+    }
+    newArr[size] = Trip(tripNum, dest, Date(day, month, year));
+    delete[] arr;
+    size++;
+    return newArr;
+}
+
+
